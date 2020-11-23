bac65b5b84e29022ab0b7b1acb47a2ecb60ea1e0
klickers <bluebirid228@gmail.com>
2020-11-23 12:05:17 -0800

    Update course-card.blade.php

--- resources/views/components/course-card.blade.php
+++ resources/views/components/course-card.blade.php
@@ -1,10 +1,12 @@
-<div class="card">
-    <div class="card-image">
-        <img src="{{ $course->card_image }}" />
+<a href='{{ url("/courses/$course->id/$course->title") }}'>
+    <div class="card">
+        <div class="card-image">
+            <img src="{{ $course->card_image }}" />
+        </div>
+        <div class="card-content">
+            <p class="eyebrow">{{ $course->type }}</p>
+            <h5>{{ $course->title }}</h5>
+            <p>{{ $course->excerpt }}</p>
+        </div>
     </div>
-    <div class="card-content">
-        <p class="eyebrow">{{ $course->type }}</p>
-        <h5>{{ $course->title }}</h5>
-        <p>{{ $course->excerpt }}</p>
-    </div>
-</div>
+</a>
